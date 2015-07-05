42a866ba2563a31b80caa676653dcc3c3ae049cd
syrotynin <sergsnew@gmail.com>
2015-07-05 21:39:35 +0400

    git init iOS

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by admin on 05.07.15.
+//  Copyright (c) 2015 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
